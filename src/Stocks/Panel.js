779809fe06d657a9a25cc641affc8e4d3f0a1174
Amoroth <Amoroth36@gmail.com>
2018-11-15 14:14:17 +0100

    Panel.js removed emotion

--- src/Stocks/Panel.js
+++ src/Stocks/Panel.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { css } from 'emotion'
+
+import styles from './Stocks.module.css'
 
 const panel = (props) => {
   let priceChange = `${props.change > 0 ? '+' : ''}${(props.change * 100).toFixed(2)}%`
@@ -7,13 +8,13 @@ const panel = (props) => {
   if (stockName.length > 30)
   stockName = stockName.slice(0, 30) + '...'
   return (
-    <div className={style}>
+    <div className={styles['panel']}>
       <h6 
-        className={titleStyle} 
+        className={styles['panel-title']} 
         title={ stockName[stockName.length - 2] === '.' ? props.title : null }>
         { stockName }
       </h6>
-      <div>
+      <div className={styles['panel-details']}>
         <span>{ props.short }</span>
         <span>{ props.price } USD</span>
         <span style={{color: props.change > 0 ? 'green' : 'red'}}>
@@ -23,44 +24,5 @@ const panel = (props) => {
     </div>
   )
 }
-
-const style = css({
-  backgroundColor: '#fff',
-  borderRadius: '5px',
-  boxShadow: '2px 4px 10px rgba(0,0,0,.0125)',
-  fontSize: '15px',
-  margin: '4px 4px 4px 4px',
-  maxWidth: '720px',
-  padding: '16px',
-  transition: 'box-shadow .1s ease-in-out',
-  width: '48%',
-  'div': {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-  '&:hover': {
-    boxShadow: '2px 4px 10px rgba(0,0,0,.1)',
-  },
-  '@media screen and (max-width: 800px)': {
-    width: '60%',
-    maxWidth: '97%'
-  },
-  '@media screen and (max-width: 620px)': {
-    width: '100%',
-    maxWidth: '97%'
-  },
-})
-
-const titleStyle = css({
-  fontSize: '21px',
-  fontWeight: 'bold',
-  margin: '0 0 10px 0',
-})
-
-// const shortStyle = css({
-//   fontSize: '15px',
-//   fontWeight: 'normal',
-//   marginLeft: '8px',
-// })
 
 export default panel
