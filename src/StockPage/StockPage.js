918a17ca63b580c989f0085cabf40d9239a31430
Amoroth <Amoroth36@gmail.com>
2018-11-15 16:57:41 +0100

    StockPage.js short color change and back button is bigger

--- src/StockPage/StockPage.js
+++ src/StockPage/StockPage.js
@@ -4,16 +4,16 @@ import styles from './StockPage.module.css'
 
 class StockPage extends Component {
   state = {
-    name: '',
+    name: 'International Business Machines Corporation',
     short: '',
-    price: 0,
-    change: 0
+    price: 159.51,
+    change: 0.0241
   }
 
   componentDidMount() {
     const comp = new URLSearchParams(this.props.location.search.slice(1)).get('comp')
     this.setState(() => {
-      return {name: comp}
+      return {short: comp}
     })
   }
 
@@ -22,11 +22,15 @@ class StockPage extends Component {
       (
         <div className={styles['container']}>
           <div className={styles['title-bar']}>
-            <h6>{ this.state.name }</h6>
+            <div>
+              <h6>{ this.state.name }</h6>
+              <span>Nasdaq Global Select: { this.state.short }</span>
+            </div>
             <button onClick={this.props.history.goBack}>
-              <i class="material-icons">arrow_back</i>
+              <i class="material-icons md-48">arrow_back</i>
             </button>
           </div>
+          <hr />
           <div>
 
           </div>
