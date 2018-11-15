a2225a0fe7b438daaea6dda2f139886568249b93
Amoroth <Amoroth36@gmail.com>
2018-11-15 14:26:57 +0100

    Header.js removed emotion

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,65 +1,21 @@
 import React from 'react'
-import { css } from 'emotion'
+
+import styles from './Header.module.css'
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 const header = (props) => {
   return (
-    <nav className={style}>
+    <nav className={styles['container']}>
       <div>
-        <h6>Stock Explorer!</h6>
+        <h6 className={styles['brand']}>Stock Explorer!</h6>
       </div>
-      <div className={navStyle}>
-        <a href="#" className={navStyleActive}>Place One</a>
+      <div className={styles['nav-links']}>
+        <a href="#" className={styles['nav-link-active']}>Place One</a>
         <a href="#">Place Two</a>
       </div>
     </nav>
   )
 }
-
-const style = css({
-  alignItems: 'center',
-  // backgroundColor: '#2f2f2f',
-  backgroundColor: '#344955',
-  boxShadow: '2px 4px 10px rgba(0,0,0,.2)',
-  display: 'flex',
-  height: 62,
-  justifyContent: 'space-between',
-  position: 'fixed',
-  top: 0,
-  width: '100%',
-  'h6': {
-    color: '#fff',
-    fontSize: 22,
-    margin: '0 15px'
-  },
-})
-
-const navStyle = css({
-  height: '100%',
-  margin: '0 15px',
-  'a': {
-    color: '#fff',
-    display: 'inline-block',
-    height: '100%',
-    lineHeight: '60px',
-    marginLeft: 15,
-    position: 'relative',
-    textDecoration: 'none'
-  },
-  'a:hover': {
-    color: '#ddd'
-  },
-  '@media screen and (max-width: 640px)': {
-    visibility: 'hidden',
-  }
-})
-
-const navStyleActive = css({
-  borderBottom: '5px solid #fff',
-  '&:hover': {
-    borderBottom: '5px solid #ddd'
-  }
-})
 
 export default header
