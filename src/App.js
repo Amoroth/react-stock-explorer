faec49f8c387d74faef82fd4442f8b0e08c50e84
Amoroth <Amoroth36@gmail.com>
2018-11-15 15:35:21 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 
 import Header from './Header/Header'
 import Stocks from './Stocks/Stocks'
 
 class App extends Component {
   render() {
+    let router = (
+      <Switch>
+        <Route path="/" exact component={Stocks} />
+        <Route path="/*" render={() => <h1>Something else</h1>} />
+      </Switch>
+    )
+
     return (
       <div className="App">
         <Header />
-        <Stocks>
-        </Stocks>
+        {router}
       </div>
     );
   }
