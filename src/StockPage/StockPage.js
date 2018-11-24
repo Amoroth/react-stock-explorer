e88c0c0b5970a11f21ef7f5d38291344ed6a873d
Amoroth <Amoroth36@gmail.com>
2018-11-24 17:40:38 +0100

    StockPage.js hooked up api

--- src/StockPage/StockPage.js
+++ src/StockPage/StockPage.js
@@ -10,6 +10,22 @@ class StockPage extends Component {
     short: '',
     price: 159.51,
     change: 0.0241,
+    book: {
+      symbol: '',
+      companyName: '',
+      primaryExchange: '',
+      sector: '',
+      open: 0,
+      openTime: 0,
+      close: 0,
+      closeTime: 0,
+      high: 0,
+      low: 0,
+      previousClose: 0,
+      change: 0,
+      week52High: 0,
+      week52Low: 0
+    },
     chart: [{
       "date": "2018-10-22",
       "close": 219.8828,
@@ -29,29 +45,34 @@ class StockPage extends Component {
       "date": "2018-10-25",
       "close": 219.0358,
       "label": "Oct 25"
-      }]
+    }],
+    chartTime: 0
   }
 
   componentDidMount() {
     const cmp = new URLSearchParams(this.props.location.search.slice(1)).get('cmp')
+    fetch(`https://api.iextrading.com/1.0/stock/${cmp}/quote?filter=symbol,companyName,primaryExchange,sector,open,close,high,low,previousClose,change,week52High,week52Low,latestUpdate,marketCap,peRatio`).then((res) => {
+      return res.json()
+    }).then((json) => {
+      this.setState({
+        book: json
+      })
+    })
+
     // fetch(`https://api.iextrading.com/1.0/stock/${cmp}/chart?filter=date,close,label`).then((res) => {
     //   return res.json()
     // }).then((json) => {
     //   this.setState({
-    //     chart: json,
-    //     short: cmp
+    //     chart: json
     //   })
     // })
-    this.setState(() => {
-      return {short: cmp}
-    })
   }
 
   componentDidUpdate() {
     const cmp = new URLSearchParams(this.props.location.search.slice(1)).get('cmp')
-    if (cmp !== this.state.short) {
+    if (cmp !== this.state.book.symbol) {
       this.setState(() => {
-        return {short: cmp}
+        return {book: {...this.state.book, symbol: cmp}}
       })
     }
   }
@@ -60,11 +81,13 @@ class StockPage extends Component {
     this.setState({
       chart: []
     })
-    fetch(`https://api.iextrading.com/1.0/stock/${this.state.short}/chart/${time}?filter=date,close,label`).then((res) => {
+    fetch(`https://api.iextrading.com/1.0/stock/${this.state.book.symbol}/chart/${time}?filter=date,close,label`).then((res) => {
       return res.json()
     }).then((json) => {
+      let timeNum = ['1m', '3m', '6m', '1y', '2y'].indexOf(time)
       this.setState({
-        chart: json
+        chart: json,
+        chartTime: timeNum
       })
     })
   }
@@ -84,8 +107,8 @@ class StockPage extends Component {
         <div className={styles['container']}>
           <div className={styles['title-bar']}>
             <div>
-              <h6>{ this.state.name }</h6>
-              <span>Nasdaq Global Select: { this.state.short }</span>
+              <h6>{ this.state.book.companyName }</h6>
+              <span>{this.state.book.primaryExchange}: { this.state.book.symbol }</span>
             </div>
             <button onClick={() => this.props.history.push('/')}>
               <i className="material-icons md-48">arrow_back</i>
@@ -94,15 +117,23 @@ class StockPage extends Component {
           <hr />
           <div>
             <div className={styles['charttime-container']}>
-              <button className={styles['charttime-button']} onClick={() => this.onSelectCharttime('1m')}>1m</button>
-              <button className={styles['charttime-button']} onClick={() => this.onSelectCharttime('3m')}>3m</button>
-              <button className={styles['charttime-button']} onClick={() => this.onSelectCharttime('6m')}>6m</button>
-              <button className={styles['charttime-button']} onClick={() => this.onSelectCharttime('1y')}>1y</button>
-              <button className={styles['charttime-button']} onClick={() => this.onSelectCharttime('2y')}>2y</button>
+              <button className={`${styles['charttime-button']} ${this.state.chartTime === 0 ? styles['charttime-button-active'] : null}`} onClick={() => this.onSelectCharttime('1m')}>1m</button>
+              <button className={`${styles['charttime-button']} ${this.state.chartTime === 1 ? styles['charttime-button-active'] : null}`} onClick={() => this.onSelectCharttime('3m')}>3m</button>
+              <button className={`${styles['charttime-button']} ${this.state.chartTime === 2 ? styles['charttime-button-active'] : null}`} onClick={() => this.onSelectCharttime('6m')}>6m</button>
+              <button className={`${styles['charttime-button']} ${this.state.chartTime === 3 ? styles['charttime-button-active'] : null}`} onClick={() => this.onSelectCharttime('1y')}>1y</button>
+              <button className={`${styles['charttime-button']} ${this.state.chartTime === 4 ? styles['charttime-button-active'] : null}`} onClick={() => this.onSelectCharttime('2y')}>2y</button>
             </div>
             { chartEl }
-            <p>Otwarcie | Maks. | Min. | Kapitalizacja | Wskaźnik C/Z</p>
-            <p>Dywidenda | Poprz. zam. | Najw./52 tyg. | Najn./52 tyg.</p>
+            <div className={styles['book-info']}>
+                <span>Open:</span> <span>{this.state.book.open} USD</span>
+                <span>Prev. Close:</span> <span>{this.state.book.previousClose} USD</span>
+                <span>Max:</span> <span>{this.state.book.high} USD</span>
+                <span>Highest/52 weeks:</span> <span>{this.state.book.week52High} USD</span>
+                <span>Min:</span> <span>{this.state.book.low} USD</span>
+                <span>Lowest/52 weeks</span> <span>{this.state.book.week52Low} USD</span>
+                <span>Market Cap</span> <span>{(this.state.book.marketCap / 1000000000).toFixed(2)} mld USD</span>
+                <span>P/E Ratio</span> <span>{this.state.book.peRatio} USD</span>
+            </div>
           </div>
           <hr />
           <div>
