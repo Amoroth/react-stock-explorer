c17fd96c83805311bc8d22c40b1cd7ac1626c6a5
Amoroth <Amoroth36@gmail.com>
2018-11-25 15:42:21 +0100

    Stocks.js api up

--- src/Stocks/Panel.js
+++ src/Stocks/Panel.js
@@ -7,7 +7,7 @@ const panel = (props) => {
   let priceChange = `${props.change > 0 ? '+' : ''}${(props.change * 100).toFixed(2)}%`
   let stockName = props.title
   if (stockName.length > 30)
-  stockName = stockName.slice(0, 30) + '...'
+    stockName = stockName.slice(0, 28) + '...'
 
   const openFullPage = () => {
     props.history.push(`/stock?cmp=${props.short}`)
@@ -15,11 +15,18 @@ const panel = (props) => {
 
   return (
     <div className={styles['panel']} onClick={openFullPage}>
-      <h6 
-        className={styles['panel-title']} 
-        title={ stockName[stockName.length - 2] === '.' ? props.title : null }>
-        { stockName }
-      </h6>
+      <div className={styles['panel-upper']}>
+        <h6 
+          className={styles['panel-title']} 
+          title={ stockName[stockName.length - 2] === '.' ? props.title : null }>
+          { stockName }
+        </h6>
+        <button className={styles['panel-favorite']} onClick={(e) => props.onFav(e, props.short)}>
+          { props.favorite ? 
+            <i className={'material-icons'} style={{color: 'red'}}>favorite</i> : 
+            <i className={'material-icons'}>favorite_border</i> }
+        </button>
+      </div>
       <div className={styles['panel-details']}>
         <span>{ props.short }</span>
         <span>{ props.price } USD</span>
--- src/Stocks/Stocks.js
+++ src/Stocks/Stocks.js
@@ -9,35 +9,85 @@ class Stocks extends React.Component {
     this.state = {
       stocks: [
         {
-          name: 'Alphabet Inc',
-          short: 'GOOGL',
-          price: 1098.77,
-          change: 0.0467
+          companyName: 'Alphabet Inc',
+          symbol: 'GOOGL',
+          close: 1098.77,
+          change: 0.0467,
+          favorite: false
         },
         {
-          name: 'Microsoft Corporation',
-          short: 'MSFT',
-          price: 107.66,
-          change: 0.0377
+          companyName: 'Microsoft Corporation',
+          symbol: 'MSFT',
+          close: 107.66,
+          change: 0.0377,
+          favorite: false
         },
         {
-          name: 'International Business Machines Corporation',
-          short: 'IBM',
-          price: 114.99,
-          change: -0.0037
+          companyName: 'International Business Machines Corporation',
+          symbol: 'IBM',
+          close: 114.99,
+          change: -0.0037,
+          favorite: false
         },
         {
-          name: 'Twitter Inc',
-          short: 'TWTR',
-          price: 34.36,
-          change: -0.0112
+          companyName: 'Twitter Inc',
+          symbol: 'TWTR',
+          close: 34.36,
+          change: -0.0112,
+          favorite: false
         },
       ]
     }
   }
+
+  componentDidMount() {
+    fetch('https://api.iextrading.com/1.0/stock/market/list/infocus').then((res) => {
+      return res.json()
+    }).then((json) => {
+      this.setState({
+        stocks: json
+      })
+    })
+
+    this.loadFavorites()
+  }
+
+  loadFavorites = () => {
+    console.log('loading favorites')
+  }
+
+  onFavorite = (event, symbol) => {
+    event.stopPropagation()
+
+    console.log('added to favorites')
+
+    // let newStocks = this.state.stocks.slice()
+    // newStocks.forEach(element => {
+    //   if (element.short === symbol)
+    //     element.favorite = !element.favorite
+    // })
+    // this.setState({
+    //   stocks: newStocks
+    // }, () => {
+    //   localStorage.setItem('stock-explorer_favorites', this.state.stocks.reduce((arr, val) => {
+    //     if (val.favorite)
+    //       arr.push(val.symbol)
+    //     return arr
+    //   }, []))
+    // })
+  }
+
   render() {
     const stocks = this.state.stocks.map((val) => {
-      return <Panel title={val.name} short={val.short} price={val.price} change={val.change} key={val.short} />
+      return <Panel 
+        title={val.companyName} 
+        short={val.symbol} 
+        price={val.close} 
+        change={val.changePercent} 
+        // favorite={val.favorite}
+        key={val.symbol}
+        onFav={this.onFavorite}
+      />
     })
     return (
       <div className={styles['container']}>
