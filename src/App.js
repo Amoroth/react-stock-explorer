ab135f4cc5ccacfe2914152f993852389c3cf64f
Amoroth <Amoroth36@gmail.com>
2018-12-03 16:18:11 +0100

    App.js favorites moved from stocks to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Header from './Header/Header'
@@ -6,22 +6,46 @@ import Stocks from './Stocks/Stocks'
 import StockPage from './StockPage/StockPage'
 import CompanyPage from './CompanyPage/CompanyPage'
 
-const App = () => {
-  const router = (
-    <Switch>
-      <Route path="/" exact component={Stocks} />
-      <Route path="/stock" exact component={StockPage} />
-      <Route path="/company" exact component={CompanyPage} />
-      <Route render={() => <h1>404</h1>} />
-    </Switch>
-  )
+class App extends Component {
+  state = { favorites: [] }
 
-  return (
-    <div className="App">
-      <Header />
-      {router}
-    </div>
-  )
+  componentDidMount() {
+    const favorites = localStorage.getItem('stock-explorer_favorites')
+    this.setState({ favorites: favorites ? favorites.split(',') : [] })
+  }
+
+  onFavorite = (event, symbol) => {
+    event.stopPropagation()
+    const { favorites } = this.state
+
+    if (!favorites.includes(symbol)) {
+      const newFavorites = [...favorites, symbol]
+      this.setState({ favorites: newFavorites }, () => localStorage.setItem('stock-explorer_favorites', newFavorites))
+    } else {
+      const newFavorites = favorites.filter((val) => val !== symbol)
+      this.setState({ favorites: newFavorites }, () => localStorage.setItem('stock-explorer_favorites', newFavorites))
+    }
+  }
+
+  render() {
+    const { favorites } = this.state
+
+    const router = (
+      <Switch>
+        <Route path="/" exact render={() => <Stocks favorites={favorites} onFavorite={this.onFavorite} />} />
+        <Route path="/stock" exact component={StockPage} />
+        <Route path="/company" exact component={CompanyPage} />
+        <Route render={() => <h1>404</h1>} />
+      </Switch>
+    )
+
+    return (
+      <div className="App">
+        <Header />
+        {router}
+      </div>
+    )
+  }
 }
 
 export default App
--- src/Stocks/Stocks.js
+++ src/Stocks/Stocks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Panel from './Panel'
 import CircularButton from '../shared/CircularButton'
@@ -8,18 +9,16 @@ import styles from './Stocks.module.css'
 class Stocks extends React.Component {
   state = {
     stocks: [],
-    favorites: [],
     market: 'infocus',
   }
 
   componentDidMount() {
-    const favorites = localStorage.getItem('stock-explorer_favorites')
     const { market } = this.state
 
     fetch(`https://api.iextrading.com/1.0/stock/market/list/${market}`)
       .then((res) => res.json())
       .then((json) => {
-        this.setState({ stocks: json, favorites: favorites ? favorites.split(',') : [] })
+        this.setState({ stocks: json })
       })
   }
 
@@ -44,8 +43,8 @@ class Stocks extends React.Component {
   }
 
   loadFavorites = () => {
-    const favorites = localStorage.getItem('stock-explorer_favorites')
-    if (!favorites) {
+    const { favorites } = this.props
+    if (favorites.length < 1) {
       this.setState({
         stocks: [],
         market: 'favorites',
@@ -59,26 +58,13 @@ class Stocks extends React.Component {
         this.setState({
           stocks: newStocks,
           market: 'favorites',
-          favorites: favorites.split(','),
         })
       })
   }
 
-  onFavorite = (event, symbol) => {
-    event.stopPropagation()
-    const { favorites } = this.state
-
-    if (!favorites.includes(symbol)) {
-      const newFavorites = [...favorites, symbol]
-      this.setState({ favorites: newFavorites }, () => localStorage.setItem('stock-explorer_favorites', newFavorites))
-    } else {
-      const newFavorites = favorites.filter((val) => val !== symbol)
-      this.setState({ favorites: newFavorites }, () => localStorage.setItem('stock-explorer_favorites', newFavorites))
-    }
-  }
-
   render() {
-    const { stocks, favorites, market } = this.state
+    const { stocks, market } = this.state
+    const { favorites, onFavorite } = this.props
 
     let stockElements = <Spinner />
     if (stocks.length > 0) {
@@ -90,7 +76,7 @@ class Stocks extends React.Component {
           change={val.changePercent}
           favorite={favorites.includes(val.symbol)}
           key={val.symbol}
-          onFav={this.onFavorite}
+          onFav={onFavorite}
         />
       ))
     } else if (market === 'favorites') {
@@ -121,6 +107,11 @@ class Stocks extends React.Component {
       </div>
     )
   }
+}
+
+Stocks.propTypes = {
+  favorites: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onFavorite: PropTypes.func.isRequired,
 }
 
 export default Stocks
